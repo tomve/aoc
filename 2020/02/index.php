8f854a3f85a55c2daf5af79be0ddb83ee35ff9dd
Tomas Veneny <veneny.tomas@operatorict.cz>
2020-12-03 15:32:26 +0100

    Removed testing code from day02

--- 2020/02/index.php
+++ 2020/02/index.php
@@ -1,12 +1,13 @@
 <?php
 
+$file = file_get_contents('input1.txt');
+$lines = explode("\n", $file);
+
 function day02($part = 'one')
 {
-    $s=microtime(true);
-    $file = file_get_contents('input1.txt');
+    global $lines;
 
-    $lines = explode("\n", $file);
-    $i = $pii=0;
+    $i = 0;
 
     foreach ($lines as $line) {
         $expl = explode(':', $line);
@@ -20,56 +21,28 @@ function day02($part = 'one')
         $max = (int) $max_character[0];
         $single_character = trim($max_character[1]);
 
+        if($part === 'one') {
             $count_match = substr_count($characters, $single_character);
 
             if (($count_match >= $min) && ($count_match <= $max)) {
                 $i++;
             }
+        }
 
-
+        if ($part === 'two') {
             if (
                 isset($characters[$min - 1]) && ($characters[$min - 1] == $single_character)
                 != (isset($characters[$max - 1]) && ($characters[$max - 1] == $single_character))
             ) {
-                $pii++;
+                $i++;
             }
+        }
 
     }
 
-    $en=microtime(true);
-    return ($en-$s );
+    return $i ;
 }
 
+var_dump(day02());
 
-
-function ddd()
-{
-    $s=microtime(true);
-    $p1 = $p2 = 0;
-    foreach (file('input1.txt') as $line) {
-        preg_match('#(?<min>\d+)+\-(?<max>\d+) (?<l>\w): (?<p>\w+)#', $line, $m);
-
-        $occurrences = count_chars($m['p'])[ord($m['l'])];
-        if ($occurrences >= $m['min'] && $occurrences <= $m['max'])
-            $p1++;
-        if ($m['p'][$m['min']-1] == $m['l'] ^ $m['p'][$m['max']-1] == $m['l'])
-            $p2++;
-    }
-
-
-    $en=microtime(true);
-    return ($en-$s);
-}
-$a=[];
-for ($i=1;$i<1000;$i++){
-    $a[$i]=[day02(), ddd()];
-}
-
-$day=$dd=0;
-foreach ($a as $item_run => $item){
-    $day += $item[0];
-    $dd += $item[1];
-}
-
-var_dump($day/99);
-var_dump($dd/99);
+var_dump(day02('two'));
